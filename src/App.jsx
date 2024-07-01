e3a6965ed4261b5c0c1ec59a4f3b3210af798968
Abhay Kumar <abhaykumar714371@gmail.com>
2024-07-01 20:27:41 +0530

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { Navbar} from "./components/Navbar";
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
+import { Dashboard } from "./pages/Dashboard";
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+      <Route path="/" element={<Dashboard/>}></Route>
+    </Routes>
+    </BrowserRouter>
   )
 }
